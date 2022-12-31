b7152ee84e765194c5c320fcf2b8e08d186f02d3
Andrés Salan <35582438+andresindlp@users.noreply.github.com>
2022-12-31 22:29:39 +0100

    Add files via upload

---
+++ deboater_script.sh
@@ -0,0 +1,55 @@
+adb uninstall com.google.android.youtube
+adb uninstall com.microsoft.appmanager
+adb uninstall com.samsung.android.calendar
+adb uninstall com.samsung.android.aremoji
+adb uninstall com.microsoft.office.excel
+adb uninstall com.samsung.android.app.settings.bixby
+adb uninstall com.google.ar.core
+adb uninstall com.samsung.systemui.bixby2
+adb uninstall com.microsoft.skydrive
+adb uninstall com.samsung.android.bixby.service
+adb uninstall com.samsung.android.app.tips
+adb uninstall com.samsung.android.bixby.agent
+adb uninstall com.google.android.music
+adb uninstall com.facebook.services
+adb uninstall com.microsoft.office.word
+adb uninstall com.microsoft.office.powerpoint
+adb uninstall com.android.chrome
+adb uninstall com.google.android.videos
+adb uninstall com.sec.android.app.dexonpc
+adb uninstall com.samsung.android.arzone
+adb uninstall com.facebook.system
+adb uninstall com.samsung.android.bixby.wakeup
+adb uninstall com.samsung.android.samsungpass
+adb uninstall com.facebook.appmanager
+adb uninstall com.facebook.katana
+adb uninstall com.linkedin.android
+adb uninstall com.google.android.apps.tachyon 
+
+adb shell pm uninstall --user 0 com.google.android.youtube
+adb shell pm uninstall --user 0 com.microsoft.appmanager
+adb shell pm uninstall --user 0 com.samsung.android.calendar
+adb shell pm uninstall --user 0 com.samsung.android.aremoji
+adb shell pm uninstall --user 0 com.microsoft.office.excel
+adb shell pm uninstall --user 0 com.samsung.android.app.settings.bixby
+adb shell pm uninstall --user 0 com.google.ar.core
+adb shell pm uninstall --user 0 com.samsung.systemui.bixby2
+adb shell pm uninstall --user 0 com.microsoft.skydrive
+adb shell pm uninstall --user 0 com.samsung.android.bixby.service
+adb shell pm uninstall --user 0 com.samsung.android.app.tips
+adb shell pm uninstall --user 0 com.samsung.android.bixby.agent
+adb shell pm uninstall --user 0 com.google.android.music
+adb shell pm uninstall --user 0 com.facebook.services
+adb shell pm uninstall --user 0 com.microsoft.office.word
+adb shell pm uninstall --user 0 com.microsoft.office.powerpoint
+adb shell pm uninstall --user 0 com.android.chrome
+adb shell pm uninstall --user 0 com.google.android.videos
+adb shell pm uninstall --user 0 com.sec.android.app.dexonpc
+adb shell pm uninstall --user 0 com.samsung.android.arzone
+adb shell pm uninstall --user 0 com.facebook.system
+adb shell pm uninstall --user 0 com.samsung.android.bixby.wakeup
+adb shell pm uninstall --user 0 com.samsung.android.samsungpass
+adb shell pm uninstall --user 0 com.facebook.appmanager
+adb shell pm uninstall --user 0 com.facebook.katana
+adb shell pm uninstall --user 0 com.linkedin.android
+adb shell pm uninstall --user 0 com.google.android.apps.tachyon
